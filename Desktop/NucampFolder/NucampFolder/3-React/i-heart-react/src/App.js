c69f0ea6b94cff1131bc1cd3f0ac2a74b7f0419e
SOCRATE85 <siekapets@gmail.com>
2022-04-02 09:49:51 -0400

    React-I Heart React

---
+++ Desktop/NucampFolder/NucampFolder/3-React/i-heart-react/src/App.js
@@ -0,0 +1,28 @@
+import HeartLogo from "./heart.svg";
+import "./App.css";
+
+const message = "cool cud";
+
+const Heart = (props) => {
+  return (
+    <div className="heart">
+      <img className="heart-img" src={HeartLogo} alt="heart" />
+      <p className="heart-message">{props.msg} </p>
+    </div>
+  );
+};
+
+const Header = () => {
+  return <div className="App-header"> I ❤️ React </div>;
+};
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Heart msg={message} />
+    </>
+  );
+}
+
+export default App;
